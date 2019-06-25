83fec3f04a7abcf005910b842fc4ebc6f49867e0
Jin ho in <in362@naver.com>
2019-06-25 23:32:57 +0900

    Updates

---
+++ precache-manifest.22d9b5d9effae8fcfadc05ac9f624942.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300f8633325bd8962d9604f374d8e282",
+    "url": "/hoinfilx/index.html"
+  },
+  {
+    "revision": "1d8dffb921c62a094d02",
+    "url": "/hoinfilx/static/js/2.f04b92fc.chunk.js"
+  },
+  {
+    "revision": "be84800afff1020acb05",
+    "url": "/hoinfilx/static/js/main.9860470d.chunk.js"
+  },
+  {
+    "revision": "6f4332cafa6512fa3c0f",
+    "url": "/hoinfilx/static/js/runtime~main.8042d810.js"
+  },
+  {
+    "revision": "fbe3c4c01acb041ee13dff6e3f76bca2",
+    "url": "/hoinfilx/static/media/404.fbe3c4c0.jpg"
+  }
+]);
